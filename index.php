c529e94c7591ba81160a8111451e28fcae72af2f
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-03-14 17:04:06 +0100

    add base php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+
+$diskList = file_get_contents(__DIR__ . "/data/data.json");
+header('Content-Type: application/json');
+echo $diskList
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,8 +25,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"
-    defer></script>
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" defer>
+  </script>
   <!-- CUSTOM CSS -->
   <link rel="stylesheet" href="./css/style.css" />
 
